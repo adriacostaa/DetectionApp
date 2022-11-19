fb406e1d9c4ba2c661273577ee2cd4ca491d2d27
adriaaCosta <adria.c@outlook.com>
2022-11-19 16:22:24 -0400

    remocao de tela de apresentacao

--- app/src/main/java/com/example/detectionapp/MainActivity.java
+++ app/src/main/java/com/example/detectionapp/MainActivity.java
@@ -1,70 +1,104 @@
 package com.example.detectionapp;
 
-import static android.app.ProgressDialog.show;
-
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.media.ThumbnailUtils;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
+import android.view.animation.Animation;
 import android.widget.Toast;
+
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnCam, btnLista, btnPres;
+    private View btnCamera, btnInfo;
+    private boolean rotate = false;
+    private static final int pic_id= 1;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Detection App UARANA",Toast.LENGTH_LONG).show();
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
 
-        btnCam = (Button)findViewById(R.id.btn_cam);
-        btnLista = (Button)findViewById(R.id.btn_lista);
-        btnPres = (Button)findViewById(R.id.btn_pres);
+        final FloatingActionButton fabCam = findViewById(R.id.fab_cam);
+        final FloatingActionButton fabInfo = findViewById(R.id.fab_info);
+        final FloatingActionButton fabAdd = findViewById(R.id.fab_add);
 
-        btnCam.setOnClickListener(new View.OnClickListener() {
+        btnCamera = findViewById(R.id.btn_camera);
+        btnInfo = findViewById(R.id.btn_info);
+
+        ViewAnimation.initShowOut(btnCamera);
+        ViewAnimation.initShowOut(btnInfo);
+
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggleFabMode(view);
+            }
+        });
+
+        fabCam.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View view) {
-                // Launch camera if we have permission
-                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    HomeActivity home = new HomeActivity();
-                    home.startActivityForResult(cameraIntent, 1);
-                } else {
-                    //Request camera permission if we don't have it.
-                    requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
-                }
+                sendImage(view);
             }
         });
 
-        btnLista.setOnClickListener(new View.OnClickListener() {
+        fabInfo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent (MainActivity.this, HomeActivity.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Informacoes sobre o App", Toast.LENGTH_SHORT).show();
             }
         });
+    }
 
-        btnPres.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent (MainActivity.this, PresentationActivity.class);
-                startActivity(intent);
-            }
-        });
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void sendImage(View view){
+        // Launch camera if we have permission
+        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, pic_id);
+
+        } else {
+            //Request camera permission if we don't have it.
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+        }
+    }
+
+    private void toggleFabMode(View v){
+        rotate = ViewAnimation.rotateFab(v, !rotate);
+        if(rotate){
+            ViewAnimation.ShowIn(btnCamera);
+            ViewAnimation.ShowIn(btnInfo);
+        }else{
+            ViewAnimation.ShowOut(btnCamera);
+            ViewAnimation.ShowOut(btnInfo);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == pic_id && resultCode == RESULT_OK) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            intent.putExtra("image", image);
+            startActivity(intent);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
