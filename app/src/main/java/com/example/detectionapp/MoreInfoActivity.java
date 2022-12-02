5b3c752140f195a453057878c81a56f868514609
adriaaCosta <adria.c@outlook.com>
2022-12-01 20:54:12 -0400

    dados em tempo real

--- app/src/main/java/com/example/detectionapp/MoreInfoActivity.java
+++ app/src/main/java/com/example/detectionapp/MoreInfoActivity.java
@@ -3,6 +3,7 @@ package com.example.detectionapp;
 import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -14,7 +15,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,10 +30,6 @@ public class MoreInfoActivity extends AppCompatActivity {
     TextView txtTitle, txtInformation;
     private FirebaseFirestore firestore = FirebaseFirestore.getInstance();;
 
-    /*String imgUrl1 = "https://static.mundoeducacao.uol.com.br/mundoeducacao/2022/01/onca-pintada.jpg";
-    String imgUrl2 = "https://v5j9q4b5.rocketcdn.me/wp-content/uploads/2021/02/onca-pintada-saiba-tudo-sobre-o-felino-mais-poderoso-das-americas-960x679.jpg";
-    String imgUrl3 = "https://greenbond.com.br/wp-content/uploads/2021/09/unnamed-1.jpg";*/
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -41,32 +40,30 @@ public class MoreInfoActivity extends AppCompatActivity {
         }
 
         DocumentReference animalRef = firestore.collection("animal").document("0");
-        animalRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+        EventListener eventListener = new EventListener<DocumentSnapshot>(){
+
             @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        String information = document.getString("information");
-                        String title = document.getString("name");
-                        String url_img1 = document.getString("url_img1");
-                        String url_img2 = document.getString("url_img2");
-                        Log.d(TAG, "****INFORMACOES DO ANIMAL****" + information);
-                        Log.d(TAG, "****URL****" + url_img1);
-                        Log.d(TAG, "****URL****" + url_img2);
-                        sliderImage(title, information, url_img1, url_img2);
-                    } else {
-                        Log.d(TAG, "No such document");
-                    }
+            public void onEvent(@Nullable DocumentSnapshot document, @Nullable FirebaseFirestoreException error) {
+                if (document.exists()) {
+                    String information = document.getString("information");
+                    String title = document.getString("name");
+                    String url_img1 = document.getString("url_img1");
+                    String url_img2 = document.getString("url_img2");
+
+                    Log.d(TAG, "****INFORMACOES DO ANIMAL****" + information);
+                    Log.d(TAG, "****URL****" + url_img1);
+                    Log.d(TAG, "****URL****" + url_img2);
+                    confgSliderImage(title, information, url_img1, url_img2);
+
                 } else {
-                    Log.d(TAG, "get failed with ", task.getException());
+                    Log.d(TAG, "No such document");
                 }
             }
-        });
-
+        };
+        animalRef.addSnapshotListener(eventListener);
     }
 
-    private void sliderImage(String title, String information, String imgUrl1, String imgUrl2){
+    private void confgSliderImage(String title, String information, String imgUrl1, String imgUrl2){
         viewPager2 = findViewById(R.id.viewPager);
         txtTitle = findViewById(R.id.mi_title);
         txtInformation = findViewById(R.id.mi_information);
