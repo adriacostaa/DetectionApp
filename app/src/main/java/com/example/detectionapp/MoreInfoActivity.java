5a6a3298c2f56015cf451852c760201da78a3997
adriaaCosta <adria.c@outlook.com>
2022-12-04 15:47:38 -0400

    insercao de novo icone

--- app/src/main/java/com/example/detectionapp/MoreInfoActivity.java
+++ app/src/main/java/com/example/detectionapp/MoreInfoActivity.java
@@ -28,9 +28,6 @@ import java.util.List;
 
 public class MoreInfoActivity extends AppCompatActivity {
 
-    ViewPager2 viewPager2;
-    List<Image> imageList;
-    SliderAdapter sliderAdapter;
     TextView txtTitle, txtInformation;
     ImageSlider imgSlider;
     private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
@@ -48,7 +45,6 @@ public class MoreInfoActivity extends AppCompatActivity {
         String value;
         if (extras != null) {
             value = extras.getString("position");
-            Log.d(TAG, "****VALOR POSICAO****" + value);
 
             DocumentReference animalRef = firestore.collection("animal").document(value);
             EventListener eventListener = new EventListener<DocumentSnapshot>(){
@@ -61,14 +57,7 @@ public class MoreInfoActivity extends AppCompatActivity {
                         String url_img1 = document.getString("url_img1");
                         String url_img2 = document.getString("url_img2");
                         String url_img3 = document.getString("url_img3");
-
-                        Log.d(TAG, "****INFORMACOES DO ANIMAL****" + information);
-                        Log.d(TAG, "****URL****" + url_img1);
-                        Log.d(TAG, "****URL****" + url_img2);
-                        Log.d(TAG, "****URL****" + url_img3);
-                        //confgSliderImage(title, information, url_img1, url_img2,url_img3);
-                        confgSliderImage2(title, information, url_img1, url_img2,url_img3);
-
+                        confgSliderImage(title, information, url_img1, url_img2,url_img3);
                     } else {
                         Log.d(TAG, "No such document");
                     }
@@ -79,25 +68,7 @@ public class MoreInfoActivity extends AppCompatActivity {
     }
 
     private void confgSliderImage(String title, String information, String imgUrl1, String imgUrl2, String imgUrl3){
-        //viewPager2 = findViewById(R.id.viewPager);
-        txtTitle = findViewById(R.id.mi_title);
-        txtInformation = findViewById(R.id.mi_information);
-
-        txtTitle.setText(title);
-        txtInformation.setText(information);
-
-        imageList = new ArrayList<>();
-        imageList.add(new Image(imgUrl1));
-        imageList.add(new Image(imgUrl2));
-        imageList.add(new Image(imgUrl3));
-
-        sliderAdapter = new SliderAdapter(imageList,this);
-        viewPager2.setAdapter(sliderAdapter);
-    }
-
-    private void confgSliderImage2(String title, String information, String imgUrl1, String imgUrl2, String imgUrl3){
         imgSlider = findViewById(R.id.img_slider);
-        //viewPager2 = findViewById(R.id.viewPager);
         txtTitle = findViewById(R.id.mi_title);
         txtInformation = findViewById(R.id.mi_information);
 
